24380e37fc9d6a440d99b181dc02ae65d96d8cca
ligenhui <ligenhui21@163.com>
2019-08-21 01:35:59 +0800

    原生spring配置数据源

---
+++ dataSource/pure-spring-datasource/src/main/java/com/lgh/purespringdatasource/PureSpringDatasourceApplication.java
@@ -0,0 +1,81 @@
+package com.lgh.purespringdatasource;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.dbcp2.BasicDataSourceFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class PureSpringDatasourceApplication{
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext*.xml");
+		showBeans(applicationContext);
+		try {
+			dataSourceDemo(applicationContext);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+//		try {
+//			new PureSpringDatasourceApplication().showDataSource();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+		PureSpringDatasourceApplication application = applicationContext.getBean("pureSpringDatasourceApplication", PureSpringDatasourceApplication.class);
+		application.showDatas();
+	}
+
+	@Bean(destroyMethod = "close")
+	public DataSource dataSource() throws Exception {
+		Properties properties = new Properties();
+		properties.setProperty("driverClassName", "com.mysql.cj.jdbc.Driver");
+		properties.setProperty("url", "jdbc:mysql://localhost:3306/mybatis?serverTimezone=UTC");
+		properties.setProperty("username", "root");
+		properties.setProperty("password", "ligenhui");
+		return BasicDataSourceFactory.createDataSource(properties);
+	}
+
+	@Bean
+	public JdbcTemplate jdbcTemplate(DataSource dataSource){
+		return new JdbcTemplate(dataSource);
+	}
+
+	private static void showBeans(ApplicationContext applicationContext){
+		System.out.println(Arrays.toString(applicationContext.getBeanDefinitionNames()));
+	}
+
+	private static void dataSourceDemo(ApplicationContext applicationContext) throws SQLException {
+		PureSpringDatasourceApplication application = applicationContext.getBean("pureSpringDatasourceApplication", PureSpringDatasourceApplication.class);
+		application.showDataSource();
+	}
+
+	public void showDataSource() throws SQLException {
+		Connection connection = dataSource.getConnection();
+		System.out.println(connection);
+	}
+
+	public void showDatas(){
+		System.out.println(jdbcTemplate);
+		jdbcTemplate.queryForList("SELECT * FROM author").forEach(row -> System.out.println(row.toString()));
+	}
+
+}
